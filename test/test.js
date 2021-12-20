8f539a36895539b071703433a0d556ae491619c8
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-12-20 08:59:38 -0500

    Before, after, beforeEach, and afterEach test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,22 @@ const assert = require('assert')
 
 describe('file to be tested', () => {
     context('function to be tested', () => {
+        before(() => {
+            console.log('=======before')
+        })
+
+        after(() => {
+            console.log('=======after')
+        })
+
+        beforeEach(() => {
+            console.log('===before each')
+        })
+
+        afterEach(() => {
+            console.log('===after each')
+        })
+
         it('should do something', () => {
             assert.equal(1,1)
         })
