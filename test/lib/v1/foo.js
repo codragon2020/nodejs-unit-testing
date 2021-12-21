b9c03e28c14acd389ae67b9c677cafe5723fa587
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-12-21 06:30:15 -0500

    Setting up new folder structure to test recursive and script options

--- test/lib/v1/foo.js
+++ test/lib/v1/foo.js
@@ -1,7 +1,7 @@
 const chai = require('chai')
 const expect = chai.expect
 
-describe('chai test', () => {
+describe('foo test', () => {
     it('should compare some values', () => {
         expect(1).to.equal(1)
     })
